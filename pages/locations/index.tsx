7563c3a0f798e13fd674f7494119e7650498fed7
Anton-Osip <osipchika@gmail.com>
2025-01-08 16:03:47 +0300

    Выносим Header. Работа с Layout

--- pages/locations/index.tsx
+++ pages/locations/index.tsx
@@ -1,9 +1,9 @@
 import {LocationType, ResponseType} from "../../assets/api/rick-and-morty-api";
-import {Header} from "../../components/Header/Header";
 import {PageWrapper} from "../../components/PageWrapper/PageWrapper";
 import {dehydrate, useQuery} from "@tanstack/react-query";
 import {QueryClient} from "@tanstack/query-core";
 import {Card} from "../../components/Card/Card";
+import {getLayout} from "../../components/Layout/BaseLayout/BaseLayout";
 
 const getLocations = async () => {
     const response = await fetch('https://rickandmortyapi.com/api/location', {
@@ -33,9 +33,10 @@ const Locations = () => {
 
     return (
         <PageWrapper>
-            <Header/>
             {locationsList}
         </PageWrapper>
     )
 }
+
+Locations.getLayout = getLayout
 export default Locations;
