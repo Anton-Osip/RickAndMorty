ef0e44c9616a7d74b7ec2c056860d6d6be56f516
Anton-Osip <osipchika@gmail.com>
2025-01-10 17:53:49 +0300

    Lazy imports

--- pages/characters/index.tsx
+++ pages/characters/index.tsx
@@ -1,9 +1,15 @@
 import {API} from "assets/api/api";
 import {CharacterType, ResponseType} from "assets/api/rick-and-morty-api";
 import {PageWrapper} from "components/PageWrapper/PageWrapper";
-import {CharacterCard} from "components/Card/CharacterCard/CharacterCard";
+// import {CharacterCard} from "components/Card/CharacterCard/CharacterCard";
 import {getLayout} from "components/Layout/BaseLayout/BaseLayout";
+import dynamic from "next/dynamic";
 
+const CharacterCard = dynamic(() => import("components/Card/CharacterCard/CharacterCard")
+    .then(m => m.CharacterCard),
+    // {ssr:false,
+    // loading:()=><h1>LOADING...</h1>}
+)
 
 export const getStaticProps = async () => {
     const characters = await API.rickAndMorty.getCharacters();
